095669a738970869c49647c704ba822dad5792c8
Sam Morrison <sorrison@gmail.com>
2025-08-26 09:22:05 +1000

    Updates for epoxy
    Change-Id: Id521a91b9e1f700cb08a834269766de7fca5a381

--- varroa_dashboard/content/security/urls.py
+++ varroa_dashboard/content/security/urls.py
@@ -10,10 +10,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from django.conf.urls import url
+from django.urls import re_path
 
 from varroa_dashboard.content.security import views
 
+
 urlpatterns = [
-    url(r"^$", views.IndexView.as_view(), name="index"),
+    re_path(r"^$", views.IndexView.as_view(), name="index"),
 ]
